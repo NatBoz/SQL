74a08d61a2d71cefda15ee7790896e88533e2079
NatBoz <natboz@wp.pl>
2020-02-29 15:39:51 +0100

    no message

--- tablica2 SQL.sql
+++ tablica2 SQL.sql
@@ -67,7 +67,7 @@ KOLUMNA_2 VARCHAR(10) NOT NULL
 );
 INSERT INTO PRZYKŁADOWA_TABELA(KOLUMNA_1, KOLUMNA_2) VALUES('WARTOSC_1', 'WARTOSC_2');
 
-
+#Zadanie 4
 ALTER TABLE book1 MODIFY COLUMN title VARCHAR(128) NOT NULL;
 ALTER TABLE book1 MODIFY COLUMN author VARCHAR(128) NOT NULL;
 ALTER TABLE book1 MODIFY COLUMN isbn VARCHAR(128) NOT NULL;
@@ -76,5 +76,44 @@ ALTER TABLE book1 ALTER publisher SET DEFAULT 'nieznana';
 DESC book1;
 ALTER TABLE book1 ADD COLUMN in_stock INT;
 
+# Truncate służy do usuwania wszystkich wierszy z tabeli
+# TRUNCATE nazwa_tabeli;
+# DELETE FROM nazwa_tabeli WHERE warunek; służy do usuwania wybranych wierszy z tabeli
+#SELECT title, author FROM book1;
+#SELECT * FROM book1; wyświetla wszystkie kolumny
+# SELECT do pobierania danych
+# SELECT title AS tytuł FROM book1;
+# SELECT b.title FROM book1 AS b;
+# WHERE do filtrowania danych w tabeli
+#SELECT * FROM book1 WHERE page_count >= 1000;
+#LIKE używany w kluzulii WHERE do wyszukiwania okreslonego wzoru w kolumnie
+SELECT * FROM book1 WHERE price BETWEEN 50 AND 90;
+SELECT * FROM book1 WHERE title LIKE 'MySQL.Vademecum profesjonalisty';
+SELECT * FROM book1 WHERE title LIKE '%MySQL.Vademecum profesjonalisty%';
+SELECT DISTINCT publisher FROM book1;
 
+# LIMIT pozwala wybrac ograniczona liczbe rekordow (poczawszy od)
+SELECT publisher FROM book1 LIMIT 1,2;
+
+#ORDER BY pozwala sortowac wyniki zapytan
+SELECT * FROM book1 ORDER BY author;
+SELECT publisher, SUM(price) FROM book1 GROUP BY publisher;
+SELECT SUM(page_count) AS total_pages FROM book1;
+SELECT AVG(page_count) FROM book1;
+SELECT MIN(page_count) FROM book1;
+SELECT MAX(page_count) FROM book1;
+
+
+# Klauzula WHERE wykonuje sie przed grupowaniem, a zatem nie mozna w tej klauzulii sprecyzować
+# warunku zawierajacego funkcje grupowe. Aby taki warunek zawrzec w zapytaniu nalezy zastosowac dodatkowa klauzue
+# HAVING wraz z odpowiednim warunkiem
+SELECT count(*) as count, publisher FROM book1 GROUP BY publisher HAVING count > 1;
+
+# mozemy wykorzystac podzapytania, ktorych wynik moze stanowic zbior wykorzystywany jako zbior dla operatora WHERE
+SELECT * FROM book1 WHERE price = (SELECT min(price) FROM book1);
+
+
+#EXISTS sluzy do sprawdzania istnienia jakiegokolwiek rekordu w podzapytaniu
+#EXISTS zwraca true jesli zapytanie zwraca jeden lub wiecej rekordow
+SELECT * FROM book1 WHERE EXISTS (SELECT * FROM book1 WHERE price < 100);
 
