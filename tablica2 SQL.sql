1067535bf891057bbc4d271794a2a1aa0c2b96f2
NatBoz <natboz@wp.pl>
2020-02-29 12:49:20 +0100

    no message

--- tablica2 SQL.sql
+++ tablica2 SQL.sql
@@ -14,7 +14,7 @@ isbn VARCHAR(128),
 category VARCHAR(128),
 page_count TINYINT(128),
 publisher VARCHAR(128),
-price FLOAT
+price DECIMAL
 );
 ALTER TABLE book ADD COLUMN new_column TINYINT;
 ALTER TABLE book MODIFY COLUMN new_column TINYINT NOT NULL;
@@ -24,3 +24,23 @@ SHOW TABLES;
 DESC book;
 
 
+
+
+
+
+CREATE TABLE book1 (
+title VARCHAR(128),
+author VARCHAR(128),
+published  DATE,
+isbn VARCHAR(128),
+category VARCHAR(128),
+page_count TINYINT(128),
+publisher VARCHAR(128),
+price FLOAT
+);
+ALTER TABLE book1 ADD COLUMN new_column TINYINT;
+ALTER TABLE book1 MODIFY COLUMN new_column TINYINT NOT NULL;
+DESC book1;
+SHOW DATABASES;
+SHOW TABLES;
+DESC book1;
