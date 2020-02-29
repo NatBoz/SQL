1e4ed2e1a7911cd649204e238fbc4902e223836f
NatBoz <natboz@wp.pl>
2020-02-29 13:11:22 +0100

    dodanie ksiazek

--- tablica2 SQL.sql
+++ tablica2 SQL.sql
@@ -34,13 +34,28 @@ author VARCHAR(128),
 published  DATE,
 isbn VARCHAR(128),
 category VARCHAR(128),
-page_count TINYINT(128),
+page_count INT(128),
 publisher VARCHAR(128),
 price FLOAT
 );
 ALTER TABLE book1 ADD COLUMN new_column TINYINT;
-ALTER TABLE book1 MODIFY COLUMN new_column TINYINT NOT NULL;
+ALTER TABLE book1 MODIFY COLUMN page_count  INT;
+ALTER TABLE book1 DROP COLUMN new_column;
 DESC book1;
 SHOW DATABASES;
 SHOW TABLES;
 DESC book1;
+
+INSERT INTO book1 (title,author,published,isbn,category,page_count,publisher,price)
+VALUES ('Spring w akcji','Craig Walls','2015-08-13','978-83-283-0849-7','Programowanie java','624','Helion','89.00');
+
+
+INSERT INTO book1 (title,author,published,isbn,category,page_count,publisher,price)
+VALUES ('MySQL.Vademecum profesjonalisty','Paul DuBois','2014-03-28','978-83-246-8146-4','Baza danych','1216','Helion','149.90');
+
+SELECT * FROM book1;
+
+DELETE FROM book1 WHERE author='Wydanie IV';
+
+
+
